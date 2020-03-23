2f79e4825952205e69096fd8c9113e98c9b5d03f
Alberto Mori <albi.mo87@hotmail.it>
2020-03-23 21:46:08 +0100

    edit build task to build all projects. WIP add api call to save a new todo item

--- src/Clients/WebTodoList.Client.ReactJs/ClientApp/src/components/AddTodo.js
+++ src/Clients/WebTodoList.Client.ReactJs/ClientApp/src/components/AddTodo.js
@@ -1,15 +1,48 @@
 ﻿import React, { Component } from 'react';
 
+import NewTodoViewModel from '../viewModels/NewTodoViewModel';
+
 class AddTodo extends Component {
     static displayName = AddTodo.name;
+
+    state = {
+        newTodoText: ''
+    };
+
+    handleNewTodoChange(text) {
+        this.setState({
+            newTodoText: text
+        });
+    }
+
+    handleSubmit(evt) {
+        evt.preventDefault();
+        console.log(this.state.newTodoText);
+
+        let newTodoItemViewModel = new NewTodoViewModel(this.state.newTodoText);
+
+        fetch('https://localhost:44387/api/todo', {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify(newTodoItemViewModel)
+        }).then((response) => {
+            this.setState({
+                newTodoText: ''
+            });
+        }).catch((response) => {
+            alert('ERROR!');
+        })
+    }
 
     render() {
         return (
             <div className="col-md-12">
-                <form>
+                <form onSubmit={(evt) => this.handleSubmit(evt)}>
                     <div className="form-row">
                         <div className="input-group mb-3">
-                            <input type="text" className="form-control" placeholder="Add a task" aria-label="Add a task" aria-describedby="addTodoButton" />
+                            <input type="text" value={this.state.newTodoText} onChange={(evt) => this.handleNewTodoChange(evt.target.value)} className="form-control" placeholder="Add a task" aria-label="Add a task" aria-describedby="addTodoButton" />
                             <div className="input-group-append">
                                 <button className="btn btn-outline-secondary" type="submit" id="addTodoButton">
                                     <span className="oi oi-plus" aria-hidden="true"></span>
