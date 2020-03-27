538dfcf7fe6c1b7b5700c68a9fd344b7bb63ad11
Alberto Mori <albi.mo87@hotmail.it>
2020-03-27 19:46:30 +0100

    add flag to hide completed tasks

--- src/Clients/WebTodoList.Client.ReactJs/ClientApp/src/components/Home.js
+++ src/Clients/WebTodoList.Client.ReactJs/ClientApp/src/components/Home.js
@@ -12,17 +12,49 @@ export class Home extends Component {
     todos: null
   };
 
+  hideIfDone = false;
+
   componentDidMount() {
     this.loadTodoItems();
   }
 
-  loadTodoItems() {
-    fetch('https://localhost:44387/api/todo')
+  loadTodoItems(hideIfDone) {
+    hideIfDone = hideIfDone || false;
+
+    let url = 'https://localhost:44387/api/todo';
+    if (hideIfDone) {
+      url += '?hideIfDone=true';
+    }
+
+    fetch(url)
       .then((response) => response.json())
       .then((items) => items.map(item => ListItemViewModel.map(item.id, item.text, item.isDone)))
       .then((items) => {
         this.setState({ todos: items });
       });
+  }
+
+  markItemAsDone(item) {
+    let todoItems = this.state.todos.slice();
+    const itemIndex = todoItems.findIndex(i => i.id === item.id);
+    if (itemIndex !== -1) {
+      todoItems[itemIndex].isDone = true;
+      if (this.hideIfDone) {
+        todoItems = todoItems.filter(i => i.id !== item.id);
+      }
+    }
+
+    this.setState({ todos: todoItems });
+  }
+
+  removeItem(item) {
+    const todoItems = this.state.todos.slice().filter(i => i.id !== item.id);
+    this.setState({ todos: todoItems });
+  }
+
+  toggleCompletedItems(hideCompletedItems) {
+    this.hideIfDone = hideCompletedItems;
+    this.loadTodoItems(hideCompletedItems);
   }
 
   render () {
@@ -33,7 +65,10 @@ export class Home extends Component {
           <AddTodo onNewTodoItemAdded={() => this.loadTodoItems()} />
         </div>
         <div className="row">
-          <TodoList items={this.state.todos} />
+          <TodoList items={this.state.todos} 
+                    onTodoItemMarkedAsDone={(item) => this.markItemAsDone(item)}
+                    OnTodoItemDeleted={(item) => this.removeItem(item)}
+                    onToggleCompletedItems={(hideCompletedItems) => this.toggleCompletedItems(hideCompletedItems)} />
         </div>
       </div>
     );
--- src/Clients/WebTodoList.Client.ReactJs/ClientApp/src/components/TodoList.js
+++ src/Clients/WebTodoList.Client.ReactJs/ClientApp/src/components/TodoList.js
@@ -3,27 +3,77 @@ import React, { Component } from 'react';
 class TodoList extends Component {
     static displayName = TodoList.name;
 
+    state = {
+        hideCompletedTask: false
+    }
+
+    markTodoItemAsDone(item) {
+        const url = 'https://localhost:44387/api/todo/' + item.id + '/done';
+        fetch(url, {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: 'PATCH',
+        }).then((response) => {
+            this.props.onTodoItemMarkedAsDone(item);
+        }).catch((response) => {
+            alert('ERROR!');
+        });
+    }
+
+    deleteTodoItem(item) {
+        const url = 'https://localhost:44387/api/todo/' + item.id;
+        fetch(url, {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: 'DELETE',
+        }).then((response) => {
+            this.props.OnTodoItemDeleted(item);
+        }).catch((response) => {
+            alert('ERROR!');
+        });
+    }
+
+    toggleCompletedItems() {
+        const { hideCompletedTask } = this.state;
+        this.setState({
+            hideCompletedTask: !hideCompletedTask
+        });
+
+        this.props.onToggleCompletedItems(!hideCompletedTask);
+    }
+
     buildTodoList = () => {
         return (
             !this.props.items ?
             <div className="alert alert-info">
-                <p>Nothing to do</p>
+                <p>Nothing to do :)</p>
             </div>
             :
-            <div className="list-group">
-                {this.buildListItems(this.props.items || [])}
+            <div>
+                <div className="custom-control custom-switch">
+                    <input type="checkbox" className="custom-control-input" id="hideTaskIfDone" checked={this.state.hideCompletedTask} onChange={() => this.toggleCompletedItems()} />
+                    <label className="custom-control-label" htmlFor="hideTaskIfDone">Hide completed task</label>
+                </div>
+                <hr />
+                <div className="list-group">
+                    {this.buildListItems(this.props.items || [])}
+                </div>
             </div>
         ); 
     };
 
     buildListItems = (items) => {
         return items.map((i) => {
-            let checkboxId = i.id + "-is-done";
+            const checkboxId = i.id + "-is-done";
+            const style = i.isDone ? { textDecoration: 'line-through' } : {};
+
             return (
                 <div className="list-group-item d-flex justify-content-between align-items-center" key={i.id}>
-                    <input className="form-check-input" type="checkbox" id={checkboxId} />
-                    <label className="form-check-label" htmlFor={checkboxId}>{i.text}</label>
-                    <button type="button" className="btn btn-outline-danger" title="Delete this item">
+                    <input className="form-check-input" type="checkbox" id={checkboxId} disabled={i.isDone} defaultChecked={i.isDone} onClick={() => this.markTodoItemAsDone(i)} />
+                    <label className="form-check-label" htmlFor={checkboxId} style={style}>{i.text}</label>
+                    <button type="button" className="btn btn-outline-danger" title="Delete this item" onClick={() => this.deleteTodoItem(i)}>
                         <span>X</span>
                         <span className="sr-only">Delete</span>
                     </button>
